ffe0fc943172d55b555467618728cd7811fb0c71
masashi99 <sgmdq601@gmail.com>
2025-09-22 09:16:37 +0900

    feat: google maps for ios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
-}
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: "#fff",
+		height,
+		width,
+		justifyContent: "flex-end",
 		alignItems: "center",
-		justifyContent: "center",
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
 	},
 });
+
+export default () => (
+	<View style={styles.container}>
+		<MapView
+			provider={PROVIDER_GOOGLE}
+			style={styles.map}
+			region={{
+				latitude: 37.78825,
+				longitude: -122.4324,
+				latitudeDelta: 0.015,
+				longitudeDelta: 0.0121,
+			}}
+		></MapView>
+	</View>
+);
