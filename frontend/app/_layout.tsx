f490a4ba2550b199ef1a727fb690e6eeab1e834c
masashi99 <sgmdq601@gmail.com>
2025-09-30 22:16:11 +0900

    feat: home icon

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Layout() {
 	return (
 		<Tabs>
 			<Tabs.Screen
 				name="index"
-				options={{ title: "Home", headerShown: false }}
+				options={{
+					title: "Home",
+					headerShown: false,
+					tabBarIcon: ({ color }) => <FontAwesome name="home" size={28} color={color}/>,
+				}}
 			/>
 		</Tabs>
 	);
