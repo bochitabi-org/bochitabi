39119eb155a662228085ebf218426a4ff530fe13
masashi99 <sgmdq601@gmail.com>
2025-10-01 21:43:51 +0900

    feat: account tab

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,15 +1,32 @@
-import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Tabs } from "expo-router";
 
 export default function Layout() {
 	return (
 		<Tabs>
 			<Tabs.Screen
-				name="index"
+				name="home"
 				options={{
 					title: "Home",
 					headerShown: false,
-					tabBarIcon: ({ color }) => <FontAwesome name="home" size={28} color={color}/>,
+					tabBarIcon: ({ color }) => (
+						<FontAwesome name="home" size={28} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="account"
+				options={{
+					title: "Account",
+					headerShown: false,
+					tabBarIcon: ({ color }) => (
+						<MaterialCommunityIcons
+							name="account-circle-outline"
+							size={28}
+							color={color}
+						/>
+					),
 				}}
 			/>
 		</Tabs>
