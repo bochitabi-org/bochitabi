ad81598a24e09e3942b47d778ee089f56adc653e
masashi99 <sgmdq601@gmail.com>
2025-10-01 21:52:30 +0900

    Feature/front map tab (#6)
    * feat: add tabs
    
    * feat: home icon
    
    * feat: account tab

---
+++ frontend/app/_layout.tsx
@@ -0,0 +1,34 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Tabs } from "expo-router";
+
+export default function Layout() {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name="home"
+				options={{
+					title: "Home",
+					headerShown: false,
+					tabBarIcon: ({ color }) => (
+						<FontAwesome name="home" size={28} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="account"
+				options={{
+					title: "Account",
+					headerShown: false,
+					tabBarIcon: ({ color }) => (
+						<MaterialCommunityIcons
+							name="account-circle-outline"
+							size={28}
+							color={color}
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	);
+}
