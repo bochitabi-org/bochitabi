458e590e0836d411f8ad89655f60ade95888596c
masashi99 <sgmdq601@gmail.com>
2025-11-03 21:08:00 +0900

    add: generate models

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	// "github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+)
+
+// func main() {
+//   router := gin.Default()
+//   router.GET("/ping", func(c *gin.Context) {
+//     c.JSON(200, gin.H{
+//       "message": "pong",
+//     })
+//   })
+//   router.Run() // デフォルトで0.0.0.0:8080で待機します
+// }
 
 func main() {
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
+  g := gen.NewGenerator(gen.Config{
+    OutPath: ".",
+    Mode: gen.WithoutContext|gen.WithDefaultQuery|gen.WithQueryInterface,
+    ModelPkgPath: "./model",
   })
-  router.Run() // デフォルトで0.0.0.0:8080で待機します
+
+  dsn := "host=localhost user=app password=password dbname=bochitabi port=5432 sslmode=disable search_path=bochitabi"
+  db, _ := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+  
+  g.UseDB(db)
+
+  g.GenerateAllTable()
+
+  g.Execute()
 }
