3d5df8538f1f8360fa25efc57ec43087f243585b
masashi99 <sgmdq601@gmail.com>
2025-11-06 22:14:51 +0900

    tmp

--- backend/cmd/generate/main.go
+++ backend/cmd/generate/main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	// "github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gen"
 	"gorm.io/gorm"
 )
 
-// func main() {
-//   router := gin.Default()
-//   router.GET("/ping", func(c *gin.Context) {
-//     c.JSON(200, gin.H{
-//       "message": "pong",
-//     })
-//   })
-//   router.Run() // デフォルトで0.0.0.0:8080で待機します
-// }
-
 func main() {
   g := gen.NewGenerator(gen.Config{
-    OutPath: ".",
+    OutPath: "infrastructure/gen/query",
     Mode: gen.WithoutContext|gen.WithDefaultQuery|gen.WithQueryInterface,
-    ModelPkgPath: "./model",
   })
 
   dsn := "host=localhost user=app password=password dbname=bochitabi port=5432 sslmode=disable search_path=bochitabi"
@@ -29,7 +17,10 @@ func main() {
   
   g.UseDB(db)
 
-  g.GenerateAllTable()
+  all := g.GenerateAllTable()
+
+  g.ApplyBasic(all...)
 
   g.Execute()
+
 }
---
+++ backend/cmd/generate/relation/main.go
@@ -0,0 +1,27 @@
+package relation
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+
+	".../infrastructure/gen/model"
+)
+
+func main() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "infrastructure/gen/query",
+		Mode: gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface,
+	})
+
+	dsn := "host=localhost user=app password=password dbname=bochitabi port=5432 sslmode=disable search_path=bochitabi"
+  db, _ := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+
+	g.UseDB(db)
+
+	allModels := []interface{}{
+		g.GenerateModel(
+			model.
+		)
+	}
+}
