584610b03d30c2adfc4ced1aae11a1b7e079e437
masashi99 <sgmdq601@gmail.com>
2025-11-08 17:07:34 +0900

    feature: return fixed memories (#19)

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -11,6 +11,21 @@ func main() {
       "message": "pong",
     })
   })
+
+  router.GET("/v1/users/:userId/memories", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "memories": []gin.H{
+        {
+          "id": "dd640d2a-360f-4bb6-b36e-d95f96a4234b",
+          "memoryName": "omoide",
+          "stroy": "hogehoge",
+          "latitude": 36.33018692714167,
+          "longitude": 140.09567236901313,
+          "pictureUrl": "https://picsum.photos/id/13",
+        },
+      },
+    })
+  })
+
   router.Run()
 }
-
