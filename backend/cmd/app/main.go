167aa74924a87c3385be98776899e0e7abd5a1a0
masashi99 <sgmdq601@gmail.com>
2025-11-30 16:54:41 +0900

    feat: load db connection config from env

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"github.com/bochitabi-org/bochitabi/backend/internal/config"
+	"github.com/bochitabi-org/bochitabi/backend/internal/infrastructure/db"
 	"github.com/bochitabi-org/bochitabi/backend/internal/infrastructure/db/queryservice"
 	"github.com/bochitabi-org/bochitabi/backend/internal/presentation"
 	"github.com/bochitabi-org/bochitabi/backend/internal/presentation/handler"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
-var dsn = "host=localhost user=app password=password dbname=bochitabi port=5432 sslmode=disable search_path=bochitabi"
-
 func main() {
-	db, _ := gorm.Open(postgres.Open(dsn))
+	cfg := config.LoadConfig()
+	db, _ := db.NewGormDB(cfg.DB)
+
 	getMemoriesQuery := queryservice.NewGetMemoriesQueryService(db)
 	memoryHandler := handler.NewMemoryHandler(getMemoriesQuery)
 	router := presentation.NewRouter(memoryHandler)
