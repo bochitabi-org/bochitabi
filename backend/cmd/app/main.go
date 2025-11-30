62b3ec284a4579ae79f105e2b15dae57e939ade2
masashi99 <sgmdq601@gmail.com>
2025-11-30 11:16:08 +0900

    feat: db access

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -4,10 +4,15 @@ import (
 	"github.com/bochitabi-org/bochitabi/backend/internal/infrastructure/db/queryservice"
 	"github.com/bochitabi-org/bochitabi/backend/internal/presentation"
 	"github.com/bochitabi-org/bochitabi/backend/internal/presentation/handler"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var dsn = "host=localhost user=app password=password dbname=bochitabi port=5432 sslmode=disable search_path=bochitabi"
+
 func main() {
-	getMemoriesQuery := queryservice.NewGetMemoriesQueryService()
+	db, _ := gorm.Open(postgres.Open(dsn))
+	getMemoriesQuery := queryservice.NewGetMemoriesQueryService(db)
 	memoryHandler := handler.NewMemoryHandler(getMemoriesQuery)
 	router := presentation.NewRouter(memoryHandler)
 	engine := router.Init()
--- backend/internal/presentation/handler/memory.go
+++ backend/internal/presentation/handler/memory.go
@@ -26,31 +26,3 @@ func (h *MemoryHandler) GetMemories(c *gin.Context) {
 
 	c.JSON(http.StatusOK, result)
 }
-
-// func (h *MemoryHandler) GetMemories(c *gin.Context) {
-//   dsn := "host=localhost user=app password=password dbname=bochitabi port=5432 sslmode=disable search_path=bochitabi"
-// 	db, _ := gorm.Open(postgres.Open(dsn))
-
-// 	query.SetDefault(db)
-
-// 	m := query.Memory
-// 	ms, _ := m.WithContext(c.Request.Context()).Preload(m.Pictures).Find()
-
-// 	memories := make([]Memory, len(ms))
-// 	for i, memory := range ms {
-// 		pictures := make([]string, len(memory.Pictures))
-// 		for j, picture := range(memory.Pictures) {
-// 			pictures[j] = picture.URL
-// 		}
-// 		memories[i] = Memory{
-// 			ID: memory.ID,
-// 			Name: memory.Name,
-// 			Story: memory.Story,
-// 			Latitude: memory.Latitude,
-// 			Longitude: memory.Longitude,
-// 			Pictures: pictures,
-// 		}
-// 	}
-
-// 	c.JSON(200, Response{memories})
-// }
